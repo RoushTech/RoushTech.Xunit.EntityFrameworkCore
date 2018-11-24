6ed43b9915a39e5d4bddf2084dfbbc6afba54615
William Roush <william.roush@roushtech.net>
2018-11-24 01:39:19 -0500

    Additional restructuring to fix hangs

--- RoushTech.Xunit.EntityFrameworkCore/DbContextTest.cs
+++ RoushTech.Xunit.EntityFrameworkCore/DbContextTest.cs
@@ -14,49 +14,13 @@ namespace RoushTech.Xunit.EntityFrameworkCore
 
         protected IServiceProvider ServiceProvider => ServiceScope.ServiceProvider;
 
-        protected IServiceScope ServiceScope { get; private set; }
+        protected IServiceScope ServiceScope { get; set; }
         
-        protected void BeginTransaction()
-        {   
+        protected void Setup()
+        {
             DbContext.Database.BeginTransaction();
         }
-
-        protected void AddContext<T>(Action<DbContextOptionsBuilder> options) where T : DbContext
-        {
-            DatabaseConfiguration.Instance.AddContext<T>(options);
-        }
-
-        protected void SeedDatabase()
-        {
-            lock (DatabaseConfiguration.Instance.ConcurrencyLock)
-            {
-                if (DatabaseConfiguration.Instance.Seeded)
-                {
-                    return;
-                }
-                
-                Seed().Wait();
-                DatabaseConfiguration.Instance.Seeded = true;
-            }
-        }
-
-        protected abstract Task Seed();
-
-        protected void CreateServiceProvider(Action<IServiceCollection> services)
-        {
-            lock (DatabaseConfiguration.Instance.ConcurrencyLock)
-            {
-                if (DatabaseConfiguration.Instance.Services == null)
-                {
-                    services(DatabaseConfiguration.Instance.ServiceCollection);
-                    DatabaseConfiguration.Instance.CreateServiceProvider();
-                }
-            }
-
-            ServiceScope = DatabaseConfiguration.Instance.Services.CreateScope();
-            DbContext = ServiceScope.ServiceProvider.GetService<TDbContext>();
-        }
-
+ 
         public void Dispose()
         {
             DbContext?.Database?.RollbackTransaction();
